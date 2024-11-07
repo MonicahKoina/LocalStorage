a945e701559864b1f15dc76726f7a4f5ddac63ea
Monicah Koina <monicahkoina1@gmail.com>
2024-11-07 22:07:40 +0300

    DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const studentForm = document.getElementById("studentForm");
+const studentContainer = document.getElementsByClassName("students");
+const nameInput = studentForm['name'];
+const registrationInput = studentForm['registration number'];
+const ageInput = studentForm ['age'];
