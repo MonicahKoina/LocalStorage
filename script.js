63d31fd296c4c829039eff262f6d9ac784cb29c8
Monicah Koina <monicahkoina1@gmail.com>
2024-11-07 22:49:43 +0300

    created elements, added innerHTML and append

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const studentForm = document.getElementById("studentForm");
-const studentContainer = document.getElementsByClassName("students");
+const studentContainer = document.querySelector("students");
 const nameInput = studentForm['name'];
-const registrationInput = studentForm['registration number'];
+const registrationInput = studentForm['registrationNumber'];
 const ageInput = studentForm ['age'];
+
+const student = [
+  {
+    name: "Monicah",
+    registrationNumber: "F01/2024",
+    age:27
+  }
+]
+
+const createStudentElement =({name, registrationNumber, age})=>{
+    const studentDiv = document.createElement("div");
+    const studentName = document.createElement("h2");
+    const studentRegistration = document.createElement("p");
+    const studentAge =  document.createElement("p");
+
+
+    studentName.innerHTML = "Student name " + name;
+    studentRegistration.innerHTML = "Student registration Number: " + registrationNumber;
+    studentAge.innerHTML = "Student age " + age;
+
+    studentDiv.append(studentName, studentRegistration, studentAge);
+    studentContainer.appendChild(studentDiv)
+    
+}
